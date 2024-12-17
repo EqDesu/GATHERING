3fa9a08bc3943fc8c579a5056c63c89ded5b46d2
EqDesu <equillyptiandesu@gmail.com>
2024-12-17 19:38:10 +0700

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,20 @@
+{
+  "name": "Eq Jawa Ireng",
+  "short_name": "EQjawir",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#4CAF50",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "type": "image/png",
+      "sizes": "192x192"
+    },
+    {
+      "src": "icon-512.png",
+      "type": "image/png",
+      "sizes": "512x512"
+    }
+  ]
+}
