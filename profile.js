fb697ba001371bff5243df1e57f9ded96f602c94
EqDesu <equillyptiandesu@gmail.com>
2024-12-19 15:02:34 +0700

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,27 +1,21 @@
 
-  // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics, getAuth, onAuthStateChanged }from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+  // Import Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase Configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA9lmHvPKxwcgU0UUAEzxIr5jJzQma_yEo",
   authDomain: "eqjawa-cihuy.firebaseapp.com",
   projectId: "eqjawa-cihuy",
-  storageBucket: "eqjawa-cihuy.firebasestorage.app",
+  storageBucket: "eqjawa-cihuy.appspot.com",
   messagingSenderId: "881496252840",
-  appId: "1:881496252840:web:dc88ca17028a6bf7093d9d",
-  measurementId: "G-6BT98ZZ433"
+  appId: "1:881496252840:web:dc88ca17028a6bf7093d9d"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const user = Auth.currentUser
-
+const auth = getAuth(app);
 
 // Fungsi untuk memperbarui profil pengguna
 function updateProfile() {
